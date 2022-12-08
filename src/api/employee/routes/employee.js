406cbc057c829f0a5dad663fb43953737075ea5c
abhishah <abhishah0961@gmail.com>
2022-12-08 15:29:20 +0530

    Added: product & order api

--- src/api/employee/routes/employee.js
+++ src/api/employee/routes/employee.js
@@ -13,10 +13,6 @@ module.exports = createCoreRouter('api::employee.employee', {
             path: '/employees',
             handler: 'employee.find',
             config: {
-                /**
-                  The `is-admin` policy found at `./src/api/restaurant/policies/is-admin.js`
-                  is executed before the `find` action in the `Restaurant.js` controller.
-                 */
                 policies: ['api::employee.is-admin']
             }
         }
