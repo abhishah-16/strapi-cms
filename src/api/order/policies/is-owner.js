d57a58869f35ac6cf8a5b9048a64fd139869c610
abhishah <abhishah0961@gmail.com>
2022-12-09 13:29:51 +0530

    Added: comment

--- src/api/order/policies/is-owner.js
+++ src/api/order/policies/is-owner.js
@@ -8,15 +8,20 @@ const { PolicyError } = utils.errors
  */
 
 module.exports = async (policyContext, config, { strapi }) => {
-  // Add your own logic here.
   strapi.log.info('In is-owner policy.');
 
+  //* get id from params
   const { id } = policyContext.request.params
+
+  //* get user from context
   const user = policyContext.state.user
+
+  //* get order from param-id
   const order = await strapi.entityService.findOne("api::order.order", id, {
     populate: ["owner"]
   })
 
+  //* checking owner.id and user.id
   if (order.owner.id === user.id) {
     return true
   }
